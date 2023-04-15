57b9336f0e5d1507fac89c61c947ef4a22ae16aa
Will Whitehead <willclashtest@gmail.com>
2023-04-15 17:42:57 -0500

    new

--- example.py
+++ example.py
@@ -7,9 +7,12 @@ import pygame
 import cv2
 from gaze_tracking import GazeTracking
 
+x = 1000
+y = 500
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((x, y))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -46,14 +49,15 @@ while running:
         text = "Looking center"
         key = "None"
 
-        #Pygame:
+    #Pygame:
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
 
     pygame.draw.circle(screen, "red", player_pos, 20)
 
