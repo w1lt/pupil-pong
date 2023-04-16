d61406c78b3885f9ac96d3ccabf3b6e2bcf076ec
smskio <130875337+smskio@users.noreply.github.com>
2023-04-16 04:56:24 -0500

    fixed

--- menu3_old.py
+++ menu3_old.py
@@ -1,12 +1,13 @@
-#import pong
+import pong
 import cv2
-#import speech_recognition as sr
+import speech_recognition as sr
 
+gamestart = False
 main_img = cv2.resize(cv2.imread('assets/main.png'), (450,500), interpolation= cv2.INTER_LINEAR)
 about_img = cv2.resize(cv2.imread('assets/about.png'), (450,500), interpolation= cv2.INTER_LINEAR)
 window_name = 'Pupil Pong'
 cv2.imshow(window_name, main_img)
-cv2.waitKey(0)
+cv2.waitKey(1000)
 
 def show_menu(image):
     cv2.imshow(window_name, image)
@@ -31,7 +32,7 @@ def menu_voice():
         if "start" in ans:
             print("Starting game")
             cv2.destroyAllWindows()
-            #pong.main()
+            pong.main()
         elif "help" in ans:
             show_menu(about_img)
             cv2.destroyAllWindows()
@@ -39,11 +40,12 @@ def menu_voice():
             show_menu(main_img)
             cv2.destroyAllWindows()
         elif "quit" in ans:
+            gamestart = False
             print("Quitting")
             cv2.destroyAllWindows()
 
 def main():
     #show_menu(main_img)
-    while True:
+    while gamestart == False:
         menu_voice()
 main()
