35ec136e558d18d63b5054e7c082bd3574ce5959
smskio <130875337+smskio@users.noreply.github.com>
2023-04-15 22:13:01 -0500

    Update game.py

--- Bomberman/game.py
+++ Bomberman/game.py
@@ -250,18 +250,6 @@ def main(s, tile_size, show_path, terrain_images, bomb_images, explosion_images,
                 else:
                     player.frame += 1
                 
-            cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-
-            left_pupil = gaze.pupil_left_coords()
-            right_pupil = gaze.pupil_right_coords()
-            cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-            cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-
-            cv2.imshow("Demo", frame)
-
-            if cv2.waitKey(1) == 27:
-                break
-
         draw(s, grid, tile_size, show_path, game_ended, terrain_images, bomb_images, explosion_images, power_ups_images)
 
         if not game_ended:
@@ -318,7 +306,3 @@ def check_end_game():
             return False
 
     return True
-
-webcam.release()
-cv2.destroyAllWindows()
-pygame.quit()
